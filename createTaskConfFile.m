37e7d04ddc40b35fa4e012d837a06155bef1419b
Raymond Norris <raymond.norris@mathworks.com>
2022-01-19 13:11:45 -0500

    Add jobName later.

--- createTaskConfFile.m
+++ createTaskConfFile.m
@@ -1,14 +1,15 @@
 function createTaskConfFile(outputFilename, remoteQueue, remoteQslot)
 % Create the task configuration file to be loaded to the feeder
 
-
-dctSchedulerMessage(5, '%s: Creating task configuration file %s at %s', mfilename, outputFilename);
+%% RSN: TODO: Add jobName later
+% dctSchedulerMessage(5, '%s: Creating task configuration file for %s at %s', mfilename, jobName, outputFilename);
+dctSchedulerMessage(5, '%s: Creating task configuration file at %s', mfilename, outputFilename);
 
 % Open file in binary mode to make it cross-platform.
 fid = fopen(outputFilename, 'w');
 if fid < 0
     error('parallelexamples:GenericNetbatch:FileError', ...
-        'Failed to open file %s for writing', outputFilename);
+          'Failed to open file %s for writing', outputFilename);
 end
 
 fprintf(fid, 'JobsTask {\n');
