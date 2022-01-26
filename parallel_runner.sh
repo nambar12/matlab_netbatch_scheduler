28ce1552bd95998a4f32064918fd9b656f8df845
Raymond Norris <raymond.norris@mathworks.com>
2022-01-26 11:59:27 -0500

    Code cleanup.

--- parallel_runner.sh
+++ parallel_runner.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# since ssh is the default launcher, mpiexec use -x when running it
-# get rid of it since we don't need it when running the nbjob prun command
+# Since ssh is the default launcher, mpiexec use -x when running it. Get rid of it since we don't
+# need it when running the 'nbjob prun' command.
 #
-# Also, ssh launcher adds quotes to the 1st arg - hydra_pmi_proxy.
-# The ssh command escapes these qoutes. This creates bad command for the
-# nbjob prun so need to remove the quotes from all arguments
-#
+# Also, ssh launcher adds quotes to the 1st arg - 'hydra_pmi_proxy'.  The ssh command escapes these
+# qoutes. This creates a bad command for 'nbjob prun', so we need to remove the quotes from all
+# arguments.
 
 for arg do
     shift
