09e5e81896a1a5ed15d25daf1df9a44f4b303007
Raymond Norris <raymond.norris@mathworks.com>
2022-09-01 14:30:14 -0400

    Configure for 'nonshared'.

--- nonshared/createNbCluster.m
+++ nonshared/createNbCluster.m
@@ -1,11 +1,14 @@
-function nb = createNbCluster(qslot,queue)
+function nb = generateNetbatchProfile(qslot,queue)
+%GENERATENETBATCHPROFILE Create Netbatch profile
+% NB = GENERATENETBATCHPROFILE(QSLOT,QUEUE)
+%
+% Copyright 2022 The Mathworks, Inc.
+
+if nargin~=2
+   error('Must provide RemoteQslot and RemoteQueue.')
+end
 
 profile = 'netbatch';
-
-% Delete old profile if it exists
-try %#ok<TRYNC>
-    parallel.internal.ui.MatlabProfileManager.removeProfile(profile)
-end
 
 % Create generic cluster profile
 nb = parallel.cluster.Generic;
@@ -22,27 +25,86 @@ if exist(jsl,'dir')==false
 end
 
 % MW: Need to fix
-nb.ClusterMatlabRoot = matlabroot;
+nb.ClusterMatlabRoot = '';
 nb.HasSharedFilesystem = false;
 nb.JobStorageLocation = jsl;
-nb.NumThreads = 1;
+% MW: Need to fix
 nb.NumWorkers = 192;
 nb.OperatingSystem = 'unix';
-% MW: Need to fix
-nb.PluginScriptsLocation = pwd;
+nb.PluginScriptsLocation = fileparts(mfilename("fullpath"));
 
 %% AdditionalProperties
 nb.AdditionalProperties.MachineClass = 'SLES12';
 nb.AdditionalProperties.MemPerCpu = '';
-nb.AdditionalProperties.ProcsPerNode = 1;
 % MW: Need to fix
 nb.AdditionalProperties.RemoteJobStorageLocation = pwd;
 nb.AdditionalProperties.RemoteQslot = qslot;
 nb.AdditionalProperties.RemoteQueue = queue;
-nb.AdditionalProperties.UseSmpd = false;
 
 %% Save profile
+wasdefault = iDeleteOldProfile(profile);
 nb.saveAsProfile(profile)
 nb.saveProfile('Description', profile)
+if wasdefault==true
+    dp_fh = iGetClusterProfileInfo;
+    % Was previously the default profile, so set it back to the default
+    dp_fh(profile)
+end
+
+end
+
+
+function tf = iDeleteOldProfile(profile)
+
+tf = false;
+
+% Delete the profile (if it exists)
+% In order to delete the profile, check first if it's an existing profile.  If
+% so, check if it's the default profile.  If so, set the default profile to
+% "local" (otherwise, MATLAB will throw the following warning)
+%
+%  Warning: The value of DefaultProfile is 'name-of-profile-we-want-to-delete' which is not the name of an existing profile.  Setting the DefaultProfile to 'local' at the user level.  Valid profile names are:
+%  	  'local' 'profile1' 'profile2' ...
+%
+% This way, we bypass the warning message.  Then remove the old incarnation
+% of the profile (that we're going to eventually create.)
+
+[dp_fh, cp] = iGetClusterProfileInfo;
+
+if any(strcmp(profile,cp))
+    % The profile exists
+
+    % Check if it's the default profile.
+    tf = strcmp(profile,feval(dp_fh));
+
+    % Disable warning
+    state = warning('off', 'parallel:settings:CollapsedDefaultProfileNoLongerExists');
+
+    % Delete the profile
+    parallel.internal.ui.MatlabProfileManager.removeProfile(profile)
+
+    % Reset warning
+    warning(state)
+end
+
+
+end
+
+
+function [dp_fh, cp] = iGetClusterProfileInfo(~)
+
+if verLessThan('matlab','9.13')
+    % R2022a and older
+    % Handle to function returning list of cluster profiles
+    cp = parallel.clusterProfiles;
+    % Handle to function returning default cluster profile
+    dp_fh = @parallel.defaultClusterProfile;
+else
+    % R2022b and newer
+    % Handle to function returning list of cluster profiles
+    cp = parallel.listProfiles;
+    % Handle to function returning default cluster profile
+    dp_fh = @parallel.defaultProfile;
+end
 
 end
