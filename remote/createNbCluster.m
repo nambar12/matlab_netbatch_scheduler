9d5c929661c6e867eb3cad50dcbd897e63587d69
Raymond Norris <raymond.norris@mathworks.com>
2022-09-01 14:30:19 -0400

    Configure for 'nonshared'.

--- remote/createNbCluster.m
+++ remote/createNbCluster.m
@@ -1,30 +1,110 @@
-function c = createNbCluster
+function nb = generateNetbatchProfile(qslot,queue)
+%GENERATENETBATCHPROFILE Create Netbatch profile
+% NB = GENERATENETBATCHPROFILE(QSLOT,QUEUE)
+%
+% Copyright 2022 The Mathworks, Inc.
 
-profile = 'netbatch';    
-
-% Delete old profile if it exists
-try %#ok<TRYNC>
-    parallel.internal.ui.MatlabProfileManager.removeProfile(profile)
+if nargin~=2
+   error('Must provide RemoteQslot and RemoteQueue.')
 end
-    
+
+profile = 'netbatch';
+
 % Create generic cluster profile
-c = parallel.cluster.Generic;
-c.PluginScriptsLocation = pwd;
-c.HasSharedFilesystem = false;
-c.NumWorkers = 192;
-c.NumThreads = 4;
-c.OperatingSystem = 'unix';
-jsl = fullfile(getenv('HOME'),'jsl');
-if exist(jsl,'dir')~=7
-    mkdir(jsl)
+nb = parallel.cluster.Generic;
+
+rootd = fileparts(prefdir);
+release = ['R' version('-release')];
+jsl = fullfile(rootd,'3p_cluster_jobs',release);
+
+if exist(jsl,'dir')==false
+    [status,err,eid] = mkdir(jsl);
+    if status==false
+        error(eid,'Can''t make directory %s: %s',jsl,err)
+    end
 end
-c.JobStorageLocation = jsl;
-c.AdditionalProperties.MachineClass = 'SLES12&&4C';
-c.AdditionalProperties.ProcsPerNode = 2;
-c.AdditionalProperties.RemoteQslot = '/admin/nambar';
-c.AdditionalProperties.RemoteQueue = 'iil_critical';
-c.AdditionalProperties.UseSmpd = false;
-c.saveAsProfile(profile)
-c.saveProfile('Description', profile)
+
+% MW: Need to fix
+nb.ClusterMatlabRoot = '';
+nb.HasSharedFilesystem = true;
+nb.JobStorageLocation = jsl;
+% MW: Need to fix
+nb.NumWorkers = 192;
+nb.OperatingSystem = 'unix';
+nb.PluginScriptsLocation = fileparts(mfilename("fullpath"));
+
+%% AdditionalProperties
+nb.AdditionalProperties.MachineClass = 'SLES12';
+nb.AdditionalProperties.MemPerCpu = '';
+% MW: Need to fix
+nb.AdditionalProperties.RemoteJobStorageLocation = pwd;
+nb.AdditionalProperties.RemoteQslot = qslot;
+nb.AdditionalProperties.RemoteQueue = queue;
+
+%% Save profile
+wasdefault = iDeleteOldProfile(profile);
+nb.saveAsProfile(profile)
+nb.saveProfile('Description', profile)
+if wasdefault==true
+    dp_fh = iGetClusterProfileInfo;
+    % Was previously the default profile, so set it back to the default
+    dp_fh(profile)
+end
+
+end
+
+
+function tf = iDeleteOldProfile(profile)
+
+tf = false;
+
+% Delete the profile (if it exists)
+% In order to delete the profile, check first if it's an existing profile.  If
+% so, check if it's the default profile.  If so, set the default profile to
+% "local" (otherwise, MATLAB will throw the following warning)
+%
+%  Warning: The value of DefaultProfile is 'name-of-profile-we-want-to-delete' which is not the name of an existing profile.  Setting the DefaultProfile to 'local' at the user level.  Valid profile names are:
+%  	  'local' 'profile1' 'profile2' ...
+%
+% This way, we bypass the warning message.  Then remove the old incarnation
+% of the profile (that we're going to eventually create.)
+
+[dp_fh, cp] = iGetClusterProfileInfo;
+
+if any(strcmp(profile,cp))
+    % The profile exists
+
+    % Check if it's the default profile.
+    tf = strcmp(profile,feval(dp_fh));
+
+    % Disable warning
+    state = warning('off', 'parallel:settings:CollapsedDefaultProfileNoLongerExists');
+
+    % Delete the profile
+    parallel.internal.ui.MatlabProfileManager.removeProfile(profile)
+
+    % Reset warning
+    warning(state)
+end
+
+
+end
+
+
+function [dp_fh, cp] = iGetClusterProfileInfo(~)
+
+if verLessThan('matlab','9.13')
+    % R2022a and older
+    % Handle to function returning list of cluster profiles
+    cp = parallel.clusterProfiles;
+    % Handle to function returning default cluster profile
+    dp_fh = @parallel.defaultClusterProfile;
+else
+    % R2022b and newer
+    % Handle to function returning list of cluster profiles
+    cp = parallel.listProfiles;
+    % Handle to function returning default cluster profile
+    dp_fh = @parallel.defaultProfile;
+end
 
 end
